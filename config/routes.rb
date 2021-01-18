232d29c70dd1cb32976550b720e32652dd796931
EC2 Default User <ec2-user@ip-172-31-15-134.ap-northeast-1.compute.internal>
2021-01-18 07:51:49 +0000

    [Add]devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'homes#index'
+  get 'homes/about' => 'homes#about'
+
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
+
+  resources :users, only: [:show, :edit, :update, :destroy]
+
 end
