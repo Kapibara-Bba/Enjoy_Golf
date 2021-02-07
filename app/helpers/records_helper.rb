fc9116f55ba94498e1f8d0d4c9269e1473ba491c
EC2 Default User <kapibara.bba0924@gmail.com>
2021-02-07 07:32:53 +0000

    [Add]平均スコア

--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -1,2 +1,7 @@
 module RecordsHelper
+
+  def sum_score
+    self.hole1 + self.hole2 + self.hole3 + self.hole4 + self.hole5 + self.hole6 + self.hole7 + self.hole8 + self.hole9 + self.hole10 +
+    self.hole11 + self.hole12 + self.hole13 + self.hole14 + self.hole15 + self.hole16 + self.hole17 + self.hole18
+  end
 end
